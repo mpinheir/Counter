9929fb534afb24f15c3d60f45957e452cdf351d8
Marcelo Pinheiro <mpinheir@gmail.com>
2018-02-07 22:37:58 -0800

    Added add and subtract button and associated logic. First release complete.

--- app/src/main/java/com/pelware/counter/MainActivity.kt
+++ app/src/main/java/com/pelware/counter/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.pelware.counter
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +11,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    fun incrementa (view: View) {
+        // Get the text view
+        val showCountTextView= findViewById<TextView>(R.id.textView)
+
+        // Get the value of the text view.
+        val countString = showCountTextView.text.toString()
+
+        // Convert value to a number and increment it
+        var count: Int = Integer.parseInt(countString)
+        count++
+
+        // Display the new value in the text view.
+        showCountTextView.text = count.toString();
+    }
+
+    fun decrementa (view: View) {
+        // Get the text view
+        val showCountTextView= findViewById<TextView>(R.id.textView)
+
+        // Get the value of the text view.
+        val countString = showCountTextView.text.toString()
+
+        // Convert value to a number and increment it
+        var count: Int = Integer.parseInt(countString)
+        count--
+
+        // Display the new value in the text view.
+        showCountTextView.text = count.toString();
+    }
+
 }
+
