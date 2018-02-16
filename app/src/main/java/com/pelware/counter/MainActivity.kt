eedecef498979a14a8060b41259e74adb81903ad
Marcelo Pinheiro <mpinheir@gmail.com>
2018-02-15 21:50:34 -0800

    Implemented direct access to textview as per functionality provided by Kotlin.

--- app/src/main/java/com/pelware/counter/MainActivity.kt
+++ app/src/main/java/com/pelware/counter/MainActivity.kt
@@ -3,7 +3,7 @@ package com.pelware.counter
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,26 +14,20 @@ class MainActivity : AppCompatActivity() {
 
 
     fun incrementa (view: View) {
-        // Get the text view
-        val showCountTextView= findViewById<TextView>(R.id.textView)
-
         // Get the value of the text view.
-        val countString = showCountTextView.text.toString()
+        val countString = textView.text.toString()
 
         // Convert value to a number and increment it
         var count: Int = Integer.parseInt(countString)
         count++
 
         // Display the new value in the text view.
-        showCountTextView.text = count.toString();
+        textView.text = count.toString();
     }
 
     fun decrementa (view: View) {
-        // Get the text view
-        val showCountTextView= findViewById<TextView>(R.id.textView)
-
         // Get the value of the text view.
-        val countString = showCountTextView.text.toString()
+        val countString = textView.text.toString()
 
         // Decrements counter value if counter > 0
         var count: Int = Integer.parseInt(countString)
@@ -42,20 +36,13 @@ class MainActivity : AppCompatActivity() {
             count--
         }
 
-
         // Display the new value in the text view.
-        showCountTextView.text = count.toString();
+        textView.text = count.toString();
     }
 
     fun reset (view: View) {
-        // Get the text view
-        val showCountTextView= findViewById<TextView>(R.id.textView)
-
-        // Get the value of the text view.
-        val countString = showCountTextView.text.toString()
-
         // Display the new value in the text view.
-        showCountTextView.text = 0.toString();
+        textView.text = 0.toString();
     }
 
 }
